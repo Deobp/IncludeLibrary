d0e89377ba7e148117c8ca641f3712721a52f44a
Sasha <zzsanchozz@gmail.com>
2020-09-30 19:11:34 +0300

    testing promises

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let input = document.getElementById('input');
 
-
+let script = document.createElement('script');
 form.addEventListener('click', submit)
 
 function submit() {
-    let item = fetch(input.value);
-    return new Promise
 
-    function((resolve, reject) {
-            resolve(item);
-            reject('cannot upload script')
-        )
-    };
+    return new Promise((resolve, reject) => {
+        resolve(input.value)
+    });
 }
+
+let promise = submit();
+promise.then((item) => {
+    script.src = item;
+    document.head.append(item);
+    input.value = ''
+    event.preventDefault(event)
+});
+
+
+// a ) input box where we specify resource to load  =>< script src="#"> </script>
+// b) button that when presses changes # to actual URL
+// c) Alert that says resource has been loaded

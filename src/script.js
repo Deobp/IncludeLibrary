3b3994904482e6f46cc7435d5003a05bc8e3eae3
Sasha <zzsanchozz@gmail.com>
2020-10-16 16:27:42 +0300

    add all functionality

---
+++ src/script.js
@@ -0,0 +1,41 @@
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+const octokit = new Octokit({ auth: `bca20de9a9a3d6825dd2e8f1390a7ebeb44094e2` });
+let field = document.getElementById('input');
+let form = document.getElementById('form');
+let paragraph = document.getElementById('par');
+let ulList = document.getElementById("list");
+let request;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    search();
+});
+
+async function search() {
+    request = await octokit.request('GET /users/{username}/repos', {
+        username: field.value,
+    });
+    ul(request['data'], field.value);
+}
+
+function ul(itemInList, name) {
+    let par = document.createElement("p");
+    while (paragraph.firstChild) {
+        paragraph.removeChild(paragraph.firstChild);
+    }
+
+    while (ulList.firstChild) {
+        ulList.removeChild(ulList.firstChild);
+    }
+
+    par.innerText = `${name} has ${itemInList.length} repositories:`;
+    paragraph.appendChild(par);
+
+    for (let i = 0; i < itemInList.length; i++) {
+        let list = document.createElement('li');
+        list.innerText = `${itemInList[i]['name']}`;
+        ulList.appendChild(list);
+
+    }
+    field.value = '';
+}
